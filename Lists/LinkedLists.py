7a0718f5168318f53910acc00702709059df205a
Michael Pytel <michal20pytel@gmail.com>
2023-10-03 19:15:21 +0200

    all that we did on 3 Oct at uni

--- Lists/LinkedLists.py
+++ Lists/LinkedLists.py
@@ -18,10 +18,45 @@ class LinkedList:
         p.val = v
         self.head = p
 
-list = LinkedList()
-list.insert_front(5)
-list.insert_front(4)
-list.insert_front(6)
-list.print_all()
+    def insert_last(self, v):
+        if self.head is None:
+            self.head = LinkedList.Elem(v, None)
+        else:
+            p = self.head
+            while p.next is not None:
+                p = p.next
+            p.next = LinkedList.Elem(v, None)
+
+    def del_first(self):
+        if self.head is not None:
+            self.head = self.head.next
+
+    def del_max(self):
+        if self.head is None:
+            return
+        pMax = self.head
+        ppMax = None
+        p = self.head.next
+        pp = self.head
+        while p is not None:
+            if p.val > pMax.val:
+                pMax = p
+                ppMax = pp
+            pp = p
+            p = p.next
+        if ppMax is not None:
+            ppMax.next = pMax.next
+        else:
+            self.head = pMax.next
+
+
+l = LinkedList()
+l.insert_front(5)
+l.insert_front(4)
+l.insert_front(6)
+l.insert_last(6)
+l.del_first()
+l.del_max()
+l.print_all()
 
 
