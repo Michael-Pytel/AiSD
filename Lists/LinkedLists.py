04b1adc251d7d3a1f6ecb49ae743c80d3f1505a3
Michael Pytel <michal20pytel@gmail.com>
2023-10-10 23:32:50 +0200

    added methods from classes on 10 Oct

--- Lists/LinkedLists.py
+++ Lists/LinkedLists.py
@@ -4,6 +4,7 @@ class LinkedList:
             self.val = val
             self.next = next
 
+
     def __init__(self,head=None):
         self.head = head
     def print_all(self):
@@ -49,14 +50,55 @@ class LinkedList:
         else:
             self.head = pMax.next
 
+    def insert(self, v):
+        pNew = LinkedList.Elem(v,None)
+        if self.head is None:
+            self.head = pNew
+            return
 
-l = LinkedList()
-l.insert_front(5)
-l.insert_front(4)
-l.insert_front(6)
-l.insert_last(6)
-l.del_first()
-l.del_max()
-l.print_all()
+        if self.head.val >= v:
+            pNew.next = self.head
+            self.head = pNew
+            return
+
+        p = self.head
+        while p.next is not None:
+            if p.next.val >= v:
+                pNew.next = p.next
+                p.next = pNew
+                return
+            p = p.next
+        p.next = pNew
+
+    def search1(self, v):
+        p = self.head
+        while p is not None and p.val != v:
+            p = p.next
+        return p
+
+    def print_rev_rec(self):
+        def _print_rev(tmp):
+            if tmp is None:
+                return
+            _print_rev(tmp.next)
+            print(tmp.val)
+
+        _print_rev(self.head)
+
+
+if __name__ == "__main__":
+    l = LinkedList()
+    l.insert_front(5)
+    l.insert_front(4)
+    l.insert_front(6)
+    l.insert_last(6)
+    l.del_first()
+    l.del_max()
+    # l.print_all()
+    l.insert(6)
+    l.insert(8)
+    l.insert(7)
+    # l.print_all()
+    l.print_rev_rec()
 
 
